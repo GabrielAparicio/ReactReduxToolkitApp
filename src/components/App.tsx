9ffc5d4091a02ad090da6cb2c3710f486ba51ceb
GabrielAparicio <sgaparicio22@gmail.com>
2021-07-07 00:17:40 -0500

    Update general App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App: React.FC = () => {
-    return <div>Docler FE Homework</div>
+  return <div className="test">Docler FE Homework</div>;
 };
 
 export default App;
