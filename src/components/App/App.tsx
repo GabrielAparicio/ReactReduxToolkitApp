8a47061f7073234414eb18248691a488294aa685
GabrielAparicio <sgaparicio22@gmail.com>
2021-07-07 00:41:24 -0500

    Add Routes component to App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Routes from '../../Routes';
 
 const App: React.FC = () => {
-  return <div className="test">Docler FE Homework</div>;
+  return <Routes />;
 };
 
 export default App;
