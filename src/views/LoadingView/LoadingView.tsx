219456cee2d98e8b3a80eb864fbdc13df354a04d
GabrielAparicio <sgaparicio22@gmail.com>
2021-07-07 01:15:24 -0500

    Add LoadingView component to show the corresponding ui while loading

---
+++ src/views/LoadingView/LoadingView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+
+const LoadingView: React.FC = () => {
+  return (
+    <Grid container justify="center">
+      <div>Loading...</div>
+    </Grid>
+  );
+};
+
+export default LoadingView;
