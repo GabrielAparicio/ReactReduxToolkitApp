43d28a606deff2b8ed03e250918f425c00bcf6dc
GabrielAparicio <sgaparicio22@gmail.com>
2021-07-07 02:00:53 -0500

    Delete test boilerplate

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './components/App';
-
-test('renders without crashing', () => {
-    const { getByText } = render(<App />);
-    const title = getByText(/Docler FE Homework/i);
-    expect(title).toBeInTheDocument();
-});
